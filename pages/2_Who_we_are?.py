c4681bb5a90ba2155c713a50bbd7cffe927ef077
Tural Alizada <78111301+alizadeht@users.noreply.github.com>
2024-05-01 09:32:19 +0400

    Update 2_Who_we_are?.py

--- pages/2_Who_we_are?.py
+++ pages/2_Who_we_are?.py
@@ -9,43 +9,32 @@ st.header("We are:")
 st.subheader('Tural Alizada')
 st.image('https://raw.githubusercontent.com/alizadeht/wordembedding/main/pages/turalalizada.JPG', caption='Tural Alizada - Lead on data management, model architecture, and data science aspects.', width=300)
 
+st.markdown("""
+[Contact: talizada12103@ada.edu.az](mailto:talizada12103@ada.edu.az)
+""")
+
 # Asima Kochariyeva
 st.subheader('Asima Kochariyeva')
 st.image('https://raw.githubusercontent.com/alizadeht/wordembedding/main/pages/latifasgarov.jpeg', caption='Asima Kochariyeva - Managed project documentation and validation processes.', width=300)
+
+st.markdown("""
+[Contact: akochariyeva12137@ada.edu.az](mailto:akochariyeva12137@ada.edu.az)
+""")
 
 # Latif Asgarov
 st.subheader('Latif Asgarov')
 st.image('https://raw.githubusercontent.com/alizadeht/wordembedding/main/pages/latifasgarov.jpeg', caption='Latif Asgarov - Focused on data collection and preprocessing.', width=300)
 
+st.markdown(""" 
+[Contact: lasgarov12286@ada.edu.az](mailto:lasgarov12286@ada.edu.az)
+""")
+
+
+
 # Umid Suleymanov
 st.subheader('Project Advisor: Umid Suleymanov')
 st.image('https://raw.githubusercontent.com/alizadeht/wordembedding/main/pages/umidsuleymanov.jpeg', caption='Umid Suleymanov - Project Advisor.', width=300)
 
-
-st.write("""
-         Tural Alizada - BSIT 2024
-         """)
-st.write("""
-         
-         Contact => talizada12103@ada.edu.az
-         """)
-st.write("""
-         Asima Kochariyeva - BSIT 2024
-         """)
-st.write("""
-        Contact => akochariyeva12137@ada.edu.az
-         """)
-st.write("""
-         Latif Asgarov - BSIT 2024
-         """)
-st.write("""
-        Contact => lasgarov12286@ada.edu.az
-         """)
-
-st.write("""
-         Our Mentor - Umid Suleymanov
-         """)
-st.write("""
-        Contact => usuleymanov@ada.edu.az
-         """)
-
+st.markdown("""
+[Contact: usuleymanov@ada.edu.az](mailto:usuleymanov@ada.edu.az)
+""")
